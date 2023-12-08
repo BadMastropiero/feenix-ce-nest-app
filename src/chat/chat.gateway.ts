6b82cd8b569dacd81779866f96210f9944106718
BadMastropiero <mastropierro6@gmail.com>
2023-12-08 04:40:26 -0500

    Refactor greeting logic
    - Update refresh token interval in env variables

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -5,11 +5,11 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import {v4 as uuidv4} from 'uuid';
-import OpenAI from "openai";
-import {JwtService} from "@nestjs/jwt";
-import {JWTPayload} from "../auth/jwt.payload";
-import {AuthService} from "../auth/auth.service";
+import { v4 as uuidv4 } from 'uuid';
+import OpenAI from 'openai';
+import { JwtService } from '@nestjs/jwt';
+import { JWTPayload } from '../auth/jwt.payload';
+import { AuthService } from '../auth/auth.service';
 
 interface User {
   id: string;
@@ -40,34 +40,24 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     private jwtService: JwtService,
     private authService: AuthService,
-  ) {
-  }
+  ) {}
 
   async handleConnection(client) {
     // A client has connected
     try {
-      await this.authService.validateToken(client.handshake.auth.token)
+      await this.authService.validateToken(client.handshake.auth.token);
     } catch {
       client.emit('message', {
         id: uuidv4(),
         text: `Hello you, please authenticate yourself`,
         user: chatbotUser,
-        createdAt: new Date()
+        createdAt: new Date(),
       });
       client.disconnect();
       return;
     }
 
-    const user = this.extractUserFromToken(client.handshake.auth.token);
-
     this.users[client.id] = [];
-
-    client.emit('message', {
-      id: uuidv4(),
-      text: `Hello ${user?.firstName} ${user?.lastName}`,
-      user: chatbotUser,
-      createdAt: new Date()
-    });
 
     // Notify connected clients of current users
     this.server.emit('users', this.users);
@@ -79,21 +69,46 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     this.server.emit('users', this.users);
   }
-  
+
   @SubscribeMessage('message')
   async onChat(client, message) {
     const answerId = uuidv4();
-    this.users[client.id].push({id: answerId, text: message.text, user: message.user, createdAt: new Date()});
+    this.users[client.id].push({
+      id: answerId,
+      text: message.text,
+      user: message.user,
+      createdAt: new Date(),
+    });
     const stream = await this.openai.chat.completions.create({
       // messages: [{role: 'user', content: message.text}],
-      messages: this.users[client.id].map(message => ({role: 'user', content: message.text})),
+      messages: this.users[client.id].map((message) => ({
+        role: 'user',
+        content: message.text,
+      })),
       model: 'gpt-3.5-turbo',
       stream: true,
     });
     for await (const chunk of stream) {
       const response = chunk.choices[0]?.delta?.content || '';
-      client.emit('message', {id: answerId, text: response, user: chatbotUser, createdAt: new Date()});
+      client.emit('message', {
+        id: answerId,
+        text: response,
+        user: chatbotUser,
+        createdAt: new Date(),
+      });
     }
+  }
+
+  @SubscribeMessage('hello')
+  async onHello(client, message) {
+    const user = this.extractUserFromToken(client.handshake.auth.token);
+    client.emit('hello_back', {});
+    client.emit('message', {
+      id: uuidv4(),
+      text: `Hello ${user?.firstName} ${user?.lastName}, how can I help you?`,
+      user: chatbotUser,
+      createdAt: new Date(),
+    });
   }
 
   private extractUserFromToken(token: string): JWTPayload | undefined {
